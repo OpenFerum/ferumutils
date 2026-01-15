c9d8b9a7e2c41a160df8ff73065f8962a5156f5c
holos <holosnikitatt@gmail.com>
2026-01-15 16:17:30 +0200

    Update main.c

--- src/mkd/src/main.c
+++ src/mkd/src/main.c
@@ -9,8 +9,10 @@ int main(int argc, char *argv[]){
     if(strcmp(argv[1],"--help")==0){
         printf("mkd: \n");
         printf("use mkd <dir>\n");
+        return 0;
     }
     if(mkdir(argv[1], 0755)==-1){
         perror("mkd");
     }
+    return 0;
 }
