b5c9454d095dd28c66d00c0d0b959f6dd8e8e606
holos <holosnikitatt@gmail.com>
2026-01-15 16:12:58 +0200

    Create main.c

---
+++ src/rmd/src/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        printf("use rmd --help\n");
+    }
+    if(strcmp(argv[1],"--help")==0){
+        printf("rmd: \n");
+        printf("use rmd <dir>\n");
+    }
+    if(rmdir(argv[1])==-1){
+        perror("rmd");
+    }
+}
